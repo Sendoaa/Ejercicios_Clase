b10dda48fb64f7b5c03a9ff492bf9e662b3f7cfc
Sendoaa <sendoave@gmail.com>
2024-11-22 09:17:40 +0100

    Ej_6_Notas

--- Vue/Ej_6_GuardarNotas2/src/App.vue
+++ Vue/Ej_6_GuardarNotas2/src/App.vue
@@ -8,8 +8,10 @@
   <hr>
   <h2>Listado de las notas creadas</h2>
   <ul>
-    <li v-for="(nota, index) in notas" :key="index">
-      <input type="text" v-model="nota.text">
+    <li v-for="(nota, index) in notas" :key="nota.id">
+      <input type="text" v-model="nota.text" :disabled="!nota.editando">
+      <small>{{ nota.editando }}</small>
+      <button @click="editNota(nota)">{{ nota.editando ? 'No Editar' : 'Editar' }}</button>
       <button @click="deleteNota(index)">Eliminar</button>
     </li>
   </ul>
@@ -23,12 +25,19 @@ const notas = ref([])
 const newNota = ref('')
 
 const addnota = () => {
-  notas.value.push({ text: newNota.value })
+  notas.value.push({
+    text: newNota.value
+  })
   newNota.value = ''
+  editando = false
+}
+
+const editNota = (nota) => {
+  nota.editando = !nota.editando
 }
 
 const deleteNota = (index) => {
-  notas.value.splice(index)
+  notas.value.splice(index, 1)
 }
 
 </script>

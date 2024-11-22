a872ad7147023acf8c4cd3df25b171fecd408f3a
Sendoaa <sendoave@gmail.com>
2024-11-22 09:54:52 +0100

    Ej_7_Axios1

---
+++ Vue/Ej_7_Axios1/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+
+  <h1>Axios</h1>
+
+  <h2>Usuarios traidos de jsonplaceholder</h2>
+  <ul>
+    <li v-for="usuario in users" :key="usuario.id">{{ usuario.name }}</li>
+  </ul>
+
+</template>
+
+<script setup>
+
+import axios from 'axios'
+import { ref, onMounted } from 'vue'
+
+const users = ref([])
+
+onMounted(() => (
+  cargarUsers()
+))
+
+const cargarUsers = async () => {
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+    console.log(response)
+    users.value = response.data
+  } catch (error) {
+    console.error('El error devuelto es:' , error)
+  }
+}
+
+</script>

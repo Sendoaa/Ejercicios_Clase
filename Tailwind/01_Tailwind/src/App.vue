beb09dc41a8fbcf174a900be8fbf100b6ec20634
Sendoaa <sendoave@gmail.com>
2024-11-25 12:38:13 +0100

    Tailwind

---
+++ Tailwind/01_Tailwind/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+  <section class="bg-blue-200 min-h-screen grid place-content-center">
+  <div class="flex items-center max-w-sm mx-auto p-6 rounded bg-white shadow-xl">
+    <div class="shrink-0">
+      <img 
+        class="size-12" 
+        src="/img/chatlogo.png" 
+        alt="ChitChat Logo">
+    </div>
+    <div class="mx-6">
+      <h4 class="text-gray-900 text xl">ChitChat</h4>
+      <p class="text-gray-600 text-base">You have a new message!</p>
+    </div>
+  </div>
+  </section>
+</template>
+
+<style scoped></style>

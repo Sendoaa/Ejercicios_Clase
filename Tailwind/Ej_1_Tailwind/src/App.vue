afc1eab3ed9b3f32c7395bb37b5b75f1822d0d91
Sendoaa <sendoave@gmail.com>
2024-11-26 12:40:44 +0100

    Ej_Tailwind

--- Tailwind/Ej_1_Tailwind/src/App.vue
+++ Tailwind/Ej_1_Tailwind/src/App.vue
@@ -1,30 +1,326 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  
+  <!DOCTYPE html>
+
+<html lang="es">
+
+  <head>
+
+    <title>Smart Health Monitoring Wristwatch</title>
+
+    <link rel="stylesheet" href="https://unpkg.com/tailwindcss/dist/tailwind.min.css" />
+
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,700" rel="stylesheet" />
+
+  </head>
+
+  <body class="" style="font-family: 'Source Sans Pro', sans-serif">
+
+    <!--Nav-->
+
+    <nav class="">
+
+      <div class="flex justify-between m-2">
+        <a class="font-bold text-2xl text-gray-600" href="#"> SHMW </a>
+        <div class="">
+          <button class="">
+            <svg class="" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
+        <div class="space-x-7">
+          <ul class="flex space-x-7 text-gray-600 font-semibold">
+            <li>
+              <a class="" href="/">Home</a>
+            </li>
+            <li>
+              <a class="" href="#">About</a>
+            </li>
+            <li>
+              <a class="" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!--Hero-->
+
+    <div class="" style="background: linear-gradient(90deg, #667eea 0%, #764ba2 100%)">
+
+      <div class="m-3 p-10">
+
+        <h2 class="text-white font-bold text-2xl">Smart Health Monitoring Wristwatch!</h2>
+
+        <h3 class="text-white font-semibold">Monitor your health vitals smartly anywhere you go.</h3>
+
+        <button class="mt-6 bg-white text-gray-600 font-bold px-6 py-3 rounded-full shadow-lg text-sm">PRE ORDER</button>
+
+      </div>
+
+    </div>
+
+    <!-- Features -->
+
+    <section class="m-5 py-5">
+
+      <h2 class="text-center font-bold text-2xl text-gray-700">Features</h2>
+
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-12">
+
+        <div class="">
+
+          <h4 class="font-bold text-2xl text-gray-700">Exercise Metrics</h4>
+
+          <p class="text-sm text-gray-600 font-semibold">Our Smart Health Monitoring Wristwatch is able to capture you vitals while you exercise. You can create different category of exercises and can track your vitals on the go.</p>
+
+        </div>
+
+        <div class="">
+
+          <img src="https://raw.githubusercontent.com/ammezie/tailwind_landing_page/master/assets/health.svg" alt="Monitoring" />
+
+        </div>
+
+      </div>
+
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-12">
+
+        <div class="">
+
+          <img src="https://raw.githubusercontent.com/ammezie/tailwind_landing_page/master/assets/report.svg" alt="Reporting" />
+
+        </div>
+
+        <div class="m-4">
+
+          <h4 class="font-bold text-xl text-gray-800">Reporting</h4>
+
+          <p class="text-gray-600 text-sm">Our Smart Health Monitoring Wristwatch can generate a comprehensive report on your vitals depending on your settings either daily, weekly, monthly, quarterly or yearly.</p>
+
+        </div>
+
+      </div>
+
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-12">
+
+        <div class="">
+
+          <h4 class="font-bold text-xl text-gray-800">Syncing</h4>
+
+          <p class="text-gray-600 text-sm">Our Smart Health Monitoring Wristwatch allows you to sync data across all your mobile devices whether iOS, Android or Windows OS and also to your laptop whether MacOS, GNU/LInux or Windows OS.</p>
+
+        </div>
+
+        <div class="">
+
+          <img src="https://raw.githubusercontent.com/ammezie/tailwind_landing_page/master/assets/sync.svg" alt="Syncing" />
+
+        </div>
+
+      </div>
+
+    </section>
+
+    <!-- Testimonials -->
+
+    <section class="">
+
+      <div class="">
+
+        <h2 class="">Testimonials</h2>
+
+        <div class="">
+
+          <div class="">
+
+            <div class="">
+
+              <p class="">Monitoring and tracking my health vitals anywhere I go and on any platform I use has never been easier.</p>
+
+              <p class="">John Doe</p>
+
+            </div>
+
+          </div>
+
+          <div class="">
+
+            <div class="">
+
+              <p class="">As an Athlete, this is the perfect product for me. I wear my Smart Health Monitoring Wristwatch everywhere i go, even in the bathroom since it's waterproof.</p>
+
+              <p class="">Jane Doe</p>
+
+            </div>
+
+          </div>
+
+          <div class="">
+
+            <div class="">
+
+              <p class="">I don't regret buying this wearble gadget. One of the best gadgets I own!.</p>
+
+              <p class="">James Doe</p>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </section>
+
+    <!--Call to Action-->
+
+    <section style="">
+
+      <div class="">
+
+        <h2 class="">Limited in Stock</h2>
+
+        <h3 class="">Get yourself the Smart Health Monitoring Wristwatch!</h3>
+
+        <button class="">Pre Order</button>
+
+      </div>
+
+    </section>
+
+    <!--Footer-->
+
+    <footer class="">
+
+      <div class="">
+
+        <div class="">
+
+          <div class="">
+
+            <h5 class="">Links</h5>
+
+            <ul class="">
+
+              <li class="">
+
+                <a href="#" class="">FAQ</a>
+
+              </li>
+
+              <li class="">
+
+                <a href="#" class="">Help</a>
+
+              </li>
+
+              <li class="mt-2">
+
+                <a href="#" class="">Support</a>
+
+              </li>
+
+            </ul>
+
+          </div>
+
+          <div class="">
+
+            <h5 class="">Legal</h5>
+
+            <ul class="">
+
+              <li class="">
+
+                <a href="#" class="">Terms</a>
+
+              </li>
+
+              <li class="mt-2">
+
+                <a href="#" class="">Privacy</a>
+
+              </li>
+
+            </ul>
+
+          </div>
+
+          <div class="">
+
+            <h5 class="">Social</h5>
+
+            <ul class="">
+
+              <li class="">
+
+                <a href="#" class="">Facebook</a>
+
+              </li>
+
+              <li class="mt-2">
+
+                <a href="#" class="">Linkedin</a>
+
+              </li>
+
+              <li class="mt-2">
+
+                <a href="#" class="">Twitter</a>
+
+              </li>
+
+            </ul>
+
+          </div>
+
+          <div class="">
+
+            <h5 class="">Company</h5>
+
+            <ul class="">
+
+              <li class="">
+
+                <a href="#" class="">Official Blog</a>
+
+              </li>
+
+              <li class="mt-2">
+
+                <a href="#" class="">About Us</a>
+
+              </li>
+
+              <li class="mt-2">
+
+                <a href="#" class="">Contact</a>
+
+              </li>
+
+            </ul>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </footer>
+
+  </body>
+
+</html>
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style>
+
+  </style>

be9dab46338c8c391e56cc5270b3af89b7275740
Sendoaa <sendoave@gmail.com>
2024-11-29 08:19:06 +0100

    Ej_Albumes

--- Vue/Ej_explorador/src/App.vue
+++ Vue/Ej_explorador/src/App.vue
@@ -1,41 +1,74 @@
 <template>
+  <h1> Album </h1>
 
-<h1>Listado de albumes</h1>
+  <div> <!--Lista -->
+    <ul>
+      <li v-for="album in albums" :key="album.id">
 
-<ul>
-  <li v-for="album in albumes" :key="album.id">
-    <h2 style="cursor: pointer;">{{ album.title }}</h2>
-  </li>
-</ul>
+        <h2 style="cursor:pointer" @click="mostrarFotos(album.id)">{{ album.title }}</h2>
+      </li>
+    </ul>
+
+  </div>
+
+  <div id="insercion"> <!--Fotos -->
+   
+  </div>
 
 </template>
 
-<script setup>
+<style>
+a {
+  text-decoration: none;
+  color: black;
+}
+</style>
 
+<script setup>
 import axios from 'axios';
 import { ref, onMounted } from 'vue';
 
-const albumes = ref([])
+const albums = ref([]);
 
-onMounted(() => {
-  cargarAlbumes()
-})
-
-const cargarAlbumes = async () => {
+onMounted(async () => {
   try {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5');
-    albumes.value = response.data
+    const response = await axios.get('https://jsonplaceholder.typicode.com/albums?_limit=6'); //_limit=6 para que solo muestre 6
+    albums.value = response.data;
+    console.log(albums.value);
   } catch (error) {
-    console.error('Error al cargar los albumes', error)
+    console.error('Error fetching albums:', error);
   }
-}
+});
 
-const cargarfotos = async () => {
+const mostrarFotos = async (albumId) => {
   try {
-    
-  } catch (error) {
-    console.error('Error al cargar los albumes', error)
-  }
-}
+    console.log('Album ID:', albumId);
+    const responsef = await axios.get(`https://jsonplaceholder.typicode.com/albums/${albumId}/photos`);
+    console.log(responsef.data);
 
+    const photos = ref([])
+
+    photos.value = responsef.data;
+    console.log(photos.value);
+
+    const insercionDiv = document.getElementById('insercion');
+    insercionDiv.innerHTML = '';
+
+    photos.value.forEach(photo => {
+      const imgElement = document.createElement('img');
+     
+      imgElement.src = photo.url;
+
+      imgElement.alt = `Foto album ${albumId}`;
+
+      imgElement.style.width = '100px';
+      imgElement.style.height = '100px';
+      insercionDiv.appendChild(imgElement);
+    });
+
+
+  } catch (error) {
+    console.error('Error fetching album photos:', error);
+  }
+};
 </script>
